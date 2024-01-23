50872ea47660d5c330558b1c809fa58a1b14cab9
MccGrey <chuxzzie01@yahoo.com>
2024-01-23 16:56:03 +0100

    bulk commit

--- main.js
+++ main.js
@@ -34,3 +34,20 @@ const hideSidebar = () => {
 
 showSidebarBtn.addEventListener("click", showSidebar);
 hideSidebarBtn.addEventListener("click", hideSidebar);
+
+// read more functioality
+var i = 0;
+function read() {
+  if (!i) {
+    document.getElementById("more").style.display = "inline";
+    document.getElementById("dots").style.display = "none";
+    document.getElementById("read").innerHTML = "Read Less";
+
+    i = 1;
+  } else {
+    document.getElementById("more").style.display = "none";
+    document.getElementById("dots").style.display = "inline";
+    document.getElementById("read").innerHTML = "Read more";
+    i = 0;
+  }
+}
